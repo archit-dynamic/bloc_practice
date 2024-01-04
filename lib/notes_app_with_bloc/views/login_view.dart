1e2274dc770addcb2f704e53e85b47d4444fba90
archit-dynamic <architdynamic@gmail.com>
2024-01-04 12:06:54 +0530

    notes app using bloc state management

--- lib/notes_app_with_bloc/views/login_view.dart
+++ lib/notes_app_with_bloc/views/login_view.dart
@@ -2,9 +2,8 @@ import 'package:bloc_practice/notes_app_with_bloc/views/email_text_field.dart';
 import 'package:bloc_practice/notes_app_with_bloc/views/login_button.dart';
 import 'package:bloc_practice/notes_app_with_bloc/views/password_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
-class LoginView extends HookWidget {
+class LoginView extends StatelessWidget {
   final OnLoginTapped onLoginTapped;
 
   const LoginView({
@@ -14,8 +13,8 @@ class LoginView extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final emailController = useTextEditingController();
-    final passwordController = useTextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
